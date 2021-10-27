af4ca9b128fd7121705c111be44dac9de31b4cc7
TirthPinkalSheth <tirthsheth100304@gmail.com>
2021-10-27 21:30:37 +0530

    Password generator using user inputs

--- Task 1/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-a= input ("enter first name with first letter as capital")
-b= input ("enter date of birth as DDMMYYYY")
-c= input("enter special character to be used")
-password= print(a[0:2]+"c"+b)
-# concatenation is used 
-print("password is ", password)
