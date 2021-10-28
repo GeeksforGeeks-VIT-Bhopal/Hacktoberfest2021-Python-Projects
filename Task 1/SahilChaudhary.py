2d9fa0de5e3a0282c67b9d716734989c6c8812e4
Sahil Chaudhary <sahilch38@gmail.com>
2021-10-28 21:47:18 +0530

    Palindrome numbers (#39)

--- Task 1/SahilChaudhary.py
+++ Task 1/SahilChaudhary.py
@@ -5,7 +5,8 @@ Numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 LCC = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y','z']
 UCC = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'L', 'M', 'N', 'O', 'p', 'Q','R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z']
 Symbols = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>','*', '(', ')', '<']
-Maxlen = int(input("Please input the length of your desired password:\n"
+Maxlen = int(input("Please input the length of your password:\n"))
+
 # combines all the character arrays above to form one array
 COMBINED_LIST = Numbers + UCC + LCC + Symbols
 
