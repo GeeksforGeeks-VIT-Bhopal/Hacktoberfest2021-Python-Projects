2dc3a2f8bdc65e4e2b9b6bd3cc5197ff88487ea7
Sameerig <73588390+Sameerig@users.noreply.github.com>
2021-10-30 20:58:04 +0530

    kanye west quoutes using  api (#47)

---
+++ kayne/main.py
@@ -0,0 +1,30 @@
+from tkinter import *
+
+import requests
+
+def get_quote():
+    pass
+    #Write your code here.
+    responce=requests.get("https://api.kanye.rest/")
+
+    quoute= responce.json()["quote"]
+
+    canvas.itemconfig(quote_text,text =quoute)
+
+window = Tk()
+window.title("Kanye Says...")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="kayne/background.png")
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+kanye_img = PhotoImage(file="kayne\kanye.png")
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
+
+
+
+window.mainloop()
